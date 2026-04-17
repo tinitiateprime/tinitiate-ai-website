d739046b825446d84354ebc5c09cbd2cdd97d740
tinitiateprime <YOUR_EMAIL_OF_TINITIATEPRIME>
2026-04-17 16:49:37 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx'],
   safelist: ['text-white', 'bg-black', 'hover:bg-gray-800'],
   theme: {
     extend: {
